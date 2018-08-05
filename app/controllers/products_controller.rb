c5983b8dc471f21c82f30ec186de2f6e405f290d
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 16:46:49 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
   def data
     product = Product.find(params[:id])
-    render json: product.to_json
+    render json: product.to_json(include: [:id, :name, :description, :inventory, :price])
   end
 
   private
