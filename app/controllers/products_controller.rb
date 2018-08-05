e2d17048d696639c0b2f44c6f79890aa9d6ff249
Marissa Zhong <mwz2101@columbia.edu>
2018-08-05 16:48:05 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,11 +24,14 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @product.to_json(only: [:id, :name, :description, :inventory, :price])}
   end
 
   def data
     product = Product.find(params[:id])
-    render json: product.to_json(include: [:id, :name, :description, :inventory, :price])
+    render json: product.to_json(only: [:id, :name, :description, :inventory, :price])
   end
 
   private
